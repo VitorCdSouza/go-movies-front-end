9fe5181fa2f90b6d8eea2f6c9f52d6f327bb3be0
VitorCdSouza <vitorcorddesouza@gmail.com>
2024-08-14 23:15:57 -0300

    connected with back-end

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,33 +5,22 @@ const Movies = () => {
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
-        let moviesList = [
-            {
-                id: 1,
-                title: "Deadpool e Wolverine",
-                release_date: "2024-08-01",
-                runtime: 120,
-                mpaa_rating: "Adult",
-                description: "Description big"
-            },
-            {
-                id: 2,
-                title: "Borderlands",
-                release_date: "2024-08-01",
-                runtime: 120,
-                mpaa_rating: "12+",
-                description: "Worst movie"
-            },
-            {
-                id: 3,
-                title: "Borderlands",
-                release_date: "2024-08-01",
-                runtime: 120,
-                mpaa_rating: "12+",
-                description: "Worst movie"
-            },
-        ]
-        setMovies(moviesList)
+        const headers = new Headers()
+        headers.append("Content-Type", "application/json")
+
+        const requestOptions = {
+            method: "GET",
+            headers: headers
+        }
+
+        fetch(`/movies`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }, [])
 
     return (
